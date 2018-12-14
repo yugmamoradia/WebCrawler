d0510a19595032989607bf1870f2f5ce66d89539
Yugma Moradia <yugma93@gmail.com>
2018-12-13 23:17:37 -0800

    updating Param

--- src/crawler/UrlCrawler.java
+++ src/crawler/UrlCrawler.java
@@ -29,21 +29,21 @@ public class UrlCrawler {
     /**
      * Business logic to parse the links and deep links
      * @param baseUrl The start URL or the base URL where we start parsing. For making this method more customized.
-     * @param URL The URL to crawl
+     * @param Url The URL to crawl
      * @param word The word to be searched for
      * @throws SQLException
      * @throws IOException
      */
-    public static void crawl(String baseUrl, String URL, String word) throws SQLException, IOException, URISyntaxException {
+    public static void crawl(String baseUrl, String Url, String word) throws SQLException, IOException, URISyntaxException {
         if(databaseHandler.connection == null){
             System.out.println("Cannot connect to the database. Will not proceed further");
             throw new SQLException();
         }
 
         // Null check on the String params.
-        if(baseUrl != null && URL != null && word != null){
+        if(baseUrl != null && Url != null && word != null){
             //check if the given URL is already in database
-            String sql = "select * from visited where URL = '"+URL+"'";
+            String sql = "select * from visited where URL = '"+Url+"'";
             ResultSet resultSet = databaseHandler.runSelectQuery(sql);
 
             // If it doesn't exist in the Database then keep crawling
@@ -51,14 +51,14 @@ public class UrlCrawler {
                 //store the URL to database to avoid parsing again
                 sql = "INSERT INTO visited (URL) VALUES (?)";
                 PreparedStatement preparedStatement = databaseHandler.connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-                preparedStatement.setString(1, URL);
+                preparedStatement.setString(1, Url);
                 preparedStatement.execute();
 
                 // Parse and extract information from the base URL
                 Document document = Jsoup.connect(baseUrl).get();
 
                 if(document.text().contains(word)){
-                    System.out.println(URL);
+                    System.out.println(Url);
                 }
 
                 String domainName = getDomainName(baseUrl); // Example: "yahoo.com"
